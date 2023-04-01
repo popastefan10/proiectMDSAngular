756e7d7002c3c6ced15cfc1ef26d34810c68c1e5
popastefan10 <popashtefan10@gmail.com>
2023-04-01 21:02:17 +0300

    Create login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'mds-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public readonly loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
+  public ngOnInit(): void {}
+
+  public onSubmit(): void {
+    console.log('Login with the following credentials:');
+    console.log('email:', this.loginForm.controls.email.value);
+    console.log('password:', this.loginForm.controls.password.value);
+  }
 }
