f7d6ad649fb4699d65362c024c313d3810ddf989
popastefan10 <popashtefan10@gmail.com>
2023-04-09 19:59:31 +0300

    Add Angular Material to project

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule, FormsModule, RouterModule, ReactiveFormsModule],
-  exports: [CommonModule, HttpClientModule, FormsModule, RouterModule, ReactiveFormsModule]
+  imports: [CommonModule, HttpClientModule, FormsModule, RouterModule, ReactiveFormsModule, BrowserAnimationsModule],
+  exports: [CommonModule, HttpClientModule, FormsModule, RouterModule, ReactiveFormsModule, BrowserAnimationsModule]
 })
 export class SharedModule {}
