48943bfa52c8d156e34e6ca4661356d9cc1d8858
popastefan10 <popashtefan10@gmail.com>
2023-04-15 11:41:09 +0300

    Create themes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+
+const MATERIAL_MODULES = [MatSlideToggleModule, MatButtonModule];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule, FormsModule, RouterModule, ReactiveFormsModule, BrowserAnimationsModule],
-  exports: [CommonModule, HttpClientModule, FormsModule, RouterModule, ReactiveFormsModule, BrowserAnimationsModule]
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ...MATERIAL_MODULES
+  ],
+  exports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ...MATERIAL_MODULES
+  ]
 })
 export class SharedModule {}
