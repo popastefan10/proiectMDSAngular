0430b5a5c7238863d71285d7933cd850eff6976f
lowLevelGod <petrepopa19@gmail.com>
2023-04-25 12:53:55 +0300

    added requested changes

--- src/app/core/services/comment.service.ts
+++ src/app/core/services/comment.service.ts
@@ -20,24 +20,24 @@ export class CommentService {
 
   public get(id: string): Observable<GenericResponse<Partial<Comment>>> {
 
-    let url: string = '/api/comments' + '/' + id;
+    const url: string = '/api/comments' + '/' + id;
     return this.httpClient.get<GenericResponse<Partial<Comment>>>(url, { withCredentials: true });
   }
 
   public patch(data: Partial<Comment>): Observable<GenericResponse<Partial<Comment>>> {
 
-    let url: string = '/api/comments' + '/' + data.id;
+    const url: string = '/api/comments' + '/' + data.id;
     return this.httpClient.patch<GenericResponse<Partial<Comment>>>(url, data, { withCredentials: true });
   }
 
   public getCommentReplies(id: string): Observable<GenericResponse<Partial<Comment>[]>> {
 
-    let url: string = '/api/comments' + '/' + id + '/' + 'replies';
+    const url: string = '/api/comments' + '/' + id + '/' + 'replies';
     return this.httpClient.get<GenericResponse<Partial<Comment>[]>>(url, { withCredentials: true });
   }
 
   public getPostReplies(id: string): Observable<GenericResponse<Partial<Comment>[]>> {
-    let url: string = '/api/posts' + '/' + id + '/' + 'comments';
+    const url: string = '/api/posts' + '/' + id + '/' + 'comments';
     return this.httpClient.get<GenericResponse<Partial<Comment>[]>>(url, { withCredentials: true });
   }
 }
--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PostCreate } from 'app/components/post/post.create';
+import { PostCreate } from 'app/components/post/post-create.model';
 import { GenericResponse } from 'app/models/generic-response.model';
 import { Post } from 'app/models/post.model';
 // import { join } from 'path';
@@ -10,64 +10,47 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PostService {
-
-  constructor(private readonly httpClient: HttpClient) {
-
-  }
+  constructor(private readonly httpClient: HttpClient) { }
 
   public create(data: PostCreate): Observable<GenericResponse<Partial<Post>>> {
-
-    let formData = new FormData();
+    const formData = new FormData();
 
     if (data.description) {
       formData.append('description', data.description);
     }
 
-    if (data.media) {
-      [...data.media].forEach(file => {
-        formData.append('media', file);
-      });
-    }
-
+    data.media?.forEach((file) => {
+      formData.append('media', file);
+    });
     return this.httpClient.post<GenericResponse<Partial<Post>>>('/api/posts', formData, { withCredentials: true });
   }
 
   public getSinglePost(id: string): Observable<GenericResponse<Partial<Post>>> {
-
-    // let url: string = join('/api/posts', id);
-    let url: string = '/api/posts' + '/' + id;
+    const url: string = '/api/posts' + '/' + id;
     return this.httpClient.get<GenericResponse<Partial<Post>>>(url, { withCredentials: true });
   }
 
   public getPostsByUser(userId: string): Observable<GenericResponse<Partial<Post>>> {
-
     const options = {
-       params: new HttpParams().set('userid', userId),
-       withCredentials: true,
+      params: new HttpParams().set('userid', userId),
+      withCredentials: true
     };
 
     return this.httpClient.get<GenericResponse<Partial<Post>>>('/api/posts', options);
   }
 
   public delete(id: string): Observable<GenericResponse<Partial<Post>>> {
-
-    // let url: string = join('/api/posts', id);
-    let url: string = '/api/posts' + '/' + id;
+    const url: string = '/api/posts' + '/' + id;
     return this.httpClient.delete<GenericResponse<Partial<Post>>>(url, { withCredentials: true });
   }
 
   public patch(data: Partial<Post>): Observable<GenericResponse<Partial<Post>>> {
-
-    // let url: string = join('/api/posts', id);
-    let url: string = '/api/posts' + '/' + data.id;
+    const url: string = '/api/posts' + '/' + data.id;
     return this.httpClient.patch<GenericResponse<Partial<Post>>>(url, data, { withCredentials: true });
   }
 
   public getPostMedia(id: string): Observable<GenericResponse<Partial<Post>>> {
-
-    // let url: string = join('/api/posts', id);
-    let url: string = '/api/posts' + '/' + id + '/' + 'media';
+    const url: string = '/api/posts' + '/' + id + '/' + 'media';
     return this.httpClient.get<GenericResponse<Partial<Post>>>(url, { withCredentials: true });
   }
-
 }
