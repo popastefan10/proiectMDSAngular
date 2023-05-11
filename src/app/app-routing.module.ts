a100e21471cd4d49ce6920300612c94d2704c158
lowLevelGod <petrepopa19@gmail.com>
2023-05-11 12:10:52 +0300

    removed route to dummy post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ApiTestsComponent } from './components/api-tests/api-tests.component';
 import { LoginFormComponent } from './components/login/login-form.component';
 import { RegisterFormComponent } from './components/register/register-form.component';
-import { PostComponent } from './components/post/show-post/post.component';
 import { CreatePostComponent } from './components/post/create-post/create-post.component';
 import { CreateProfileComponent } from './components/create-profile/create-profile.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
@@ -15,7 +14,6 @@ const routes: Routes = [
   { path: 'login', component: LoginFormComponent },
   { path: 'register', component: RegisterFormComponent },
   { path: 'posts/create', component: CreatePostComponent},
-  { path: 'posts/:id', component: PostComponent},
   { path: 'create-profile', component: CreateProfileComponent },
   { path: 'edit-profile', component: EditProfileComponent },
   { path: 'feed', component: FeedComponent },
