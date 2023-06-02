a5d27f3ccc09ad8e9d860873fdeb56aa739366d1
popastefan10 <popashtefan10@gmail.com>
2023-06-02 18:51:26 +0300

    Refactor create post logic

--- src/app/components/post/create-post/create-post.component.ts
+++ src/app/components/post/create-post/create-post.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Post } from 'app/models/post.model';
 import { CreatePostFormType } from './create-post.type';
 import { PostCreate } from 'app/models/post-create.model';
 import { GenericResponse } from 'app/models/generic-response.model';
 import { PostService } from 'app/core/services/post.service';
-import { BehaviorSubject, Observable, combineLatest, map, startWith, tap } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, of, tap } from 'rxjs';
+import { CustomError, ErrorResponse } from 'app/shared/utils/error';
+import { openClosedAnimation } from 'app/animations';
+import { Router } from '@angular/router';
 
 interface Image {
   file: File;
@@ -17,19 +20,17 @@ const MAX_NR_IMAGES = 10;
 @Component({
   selector: 'mds-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.scss']
+  styleUrls: ['./create-post.component.scss'],
+  animations: [openClosedAnimation]
 })
 export class CreatePostComponent implements OnInit {
   maxNrFiles: number = 10;
   // use this to store file indices
   idxList: number[] = Array.from(Array(this.maxNrFiles).keys());
   uploadedFiles: (File | undefined)[] = Array(this.maxNrFiles);
-  createPostForm: FormGroup<CreatePostFormType> = this.formBuilder.nonNullable.group({
-    description: ''
-  });
 
-  private readonly images: BehaviorSubject<Image[]> = new BehaviorSubject<Image[]>([]);
-  public readonly images$ = this.images.asObservable();
+  private readonly imagesSubject: BehaviorSubject<Image[]> = new BehaviorSubject<Image[]>([]);
+  public readonly images$ = this.imagesSubject.asObservable();
   public readonly canUpload$: Observable<boolean> = this.images$.pipe(map((images) => images.length < MAX_NR_IMAGES));
   public readonly uploadMessage$: Observable<string> = this.canUpload$.pipe(
     map((canUpload) => {
@@ -38,9 +39,23 @@ export class CreatePostComponent implements OnInit {
     })
   );
 
-  constructor(private postService: PostService, private formBuilder: FormBuilder) {}
+  public readonly createPostForm: FormGroup<CreatePostFormType> = this.formBuilder.nonNullable.group({
+    description: ['', Validators.required]
+  });
+
+  public createPostError: CustomError | undefined;
+
+  constructor(
+    private readonly postService: PostService,
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {}
+
+  public get description() {
+    return this.createPostForm.controls.description;
+  }
 
   onImageUpload(file: File) {
     const reader = new FileReader();
@@ -53,9 +68,9 @@ export class CreatePostComponent implements OnInit {
           src: target.result as string
         };
 
-        const images = [...this.images.value];
+        const images = [...this.imagesSubject.value];
         images.push(newImage);
-        this.images.next(images);
+        this.imagesSubject.next(images);
       }
     };
 
@@ -63,55 +78,32 @@ export class CreatePostComponent implements OnInit {
   }
 
   onImageRemove(idx: number) {
-    const images = [...this.images.value];
+    const images = [...this.imagesSubject.value];
     images.splice(idx, 1);
-    this.images.next(images);
-  }
-
-  onFileUpload(event: Event, idx: number) {
-    const target = event.target as HTMLInputElement;
-    if (target.files && target.files.length) {
-      this.uploadedFiles[idx] = target.files[0];
-      const preview = document.getElementById('preview' + idx.toString()) as HTMLImageElement;
-      preview.src = URL.createObjectURL(target.files[0]);
-    }
-  }
-
-  onFileRemove(idx: number) {
-    const elem = document.getElementById('media' + idx.toString()) as HTMLInputElement;
-    elem.value = '';
-    this.uploadedFiles[idx] = undefined;
-    const preview = document.getElementById('preview' + idx.toString()) as HTMLImageElement;
-    preview.src = '';
+    this.imagesSubject.next(images);
   }
 
   onSubmit() {
-    const actualFiles: File[] = [];
+    if (this.createPostForm.invalid) return;
 
-    this.uploadedFiles.forEach((x) => {
-      if (x) {
-        actualFiles.push(x);
-      }
-    });
-
+    const files: File[] = this.imagesSubject.value.map((img) => img.file);
     const data: PostCreate = {
       description: this.createPostForm.value.description,
-      media: actualFiles
+      media: files
     };
 
     this.postService
       .create(data)
       .pipe(
-        tap((res: GenericResponse<Partial<Post>>) => {
-          if (res.error) {
-            console.log(res.error);
-          } else {
-            console.log(res.content);
-            this.createPostForm.reset();
-            for (let i = 0; i < this.maxNrFiles; ++i) {
-              this.onFileRemove(i);
-            }
-          }
+        tap((_res: GenericResponse<Partial<Post>>) => {
+          this.createPostForm.reset();
+          this.imagesSubject.next([]);
+          this.router.navigateByUrl('/feed');
+        }),
+        catchError((err: ErrorResponse) => {
+          this.createPostError = err.error.error;
+
+          return of(err);
         })
       )
       .subscribe();
