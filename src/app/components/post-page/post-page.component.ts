c9d3381a19e2854b41a7be468daf6d32a4afab86
popastefan10 <popashtefan10@gmail.com>
2023-06-15 10:46:02 +0300

    Create template for post page

--- src/app/components/post-page/post-page.component.ts
+++ src/app/components/post-page/post-page.component.ts
@@ -3,21 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { PostService } from 'app/core/services/post.service';
 import { Post } from 'app/models/post.model';
 import { SubscriptionCleanup } from 'app/shared/utils/subscription-cleanup';
-import {
-  BehaviorSubject,
-  Observable,
-  catchError,
-  filter,
-  map,
-  merge,
-  of,
-  scan,
-  startWith,
-  switchMap,
-  takeUntil,
-  tap,
-  withLatestFrom
-} from 'rxjs';
+import { BehaviorSubject, Observable, catchError, filter, map, of, switchMap, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'mds-post-page',
@@ -39,55 +25,15 @@ export class PostPageComponent extends SubscriptionCleanup {
       return of(undefined);
     })
   );
-  public readonly numberOfPictures$: Observable<number> = this.post$.pipe(
-    filter((post) => post !== undefined),
-    map((post) => (post ? post.picturesURLs.length : 0))
-  );
-
-  private readonly prevSubject: BehaviorSubject<void> = new BehaviorSubject<void>(undefined); // controled by left arrow
-  public readonly prev$: Observable<void> = this.prevSubject.asObservable();
-  private readonly nextSubject: BehaviorSubject<void> = new BehaviorSubject<void>(undefined); // controled by right arrow
-  public readonly next$: Observable<void> = this.nextSubject.asObservable();
-  public readonly nextPrev$: Observable<'prev' | 'next'> = merge(
-    this.prev$.pipe(map((): 'prev' => 'prev')),
-    this.next$.pipe(map((): 'next' => 'next'))
-  );
-
-  public readonly currentIndex$: Observable<number> = this.nextPrev$.pipe(
-    withLatestFrom(this.numberOfPictures$),
-    scan((idx: number, [direction, numberOfPictures]) => this.getIndex(idx, direction, numberOfPictures), 0),
-    startWith(0)
-  );
-  public readonly prevIndex$ = this.currentIndex$.pipe(map((idx) => idx - 1));
-  public readonly nextIndex$ = this.currentIndex$.pipe(map((idx) => idx + 1));
 
   constructor(private postService: PostService, private route: ActivatedRoute) {
     super();
   }
 
-  // get next or previous index, without going out of bounds
-  private getIndex(currentIndex: number, direction: 'prev' | 'next', numberOfPictures: number): number {
-    if (direction === 'prev') {
-      return Math.max(currentIndex - 1, 0);
-    }
-    return Math.min(currentIndex + 1, numberOfPictures - 1);
-  }
-
   public ngOnInit() {
-    this.post$.subscribe((post) => {
-      console.log('post', post);
-    });
     this.route.params.pipe(takeUntil(this.destroy$)).subscribe((params) => {
       const postId = params['id'];
       this.postIdSubject.next(postId);
     });
-  }
-
-  showPreviousImage() {
-    this.prevSubject.next();
-  }
-
-  showNextImage() {
-    this.nextSubject.next();
   }
 }
