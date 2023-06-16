e03f011cb2da9becd5df24dcdc23dac7b6114717
popastefan10 <popashtefan10@gmail.com>
2023-06-16 11:13:24 +0300

    Post page UI improvements and fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { PostPageComponent } from './components/post-page/post-page.component';
 import { FileUploadComponent } from './components/shared/file-upload/file-upload.component';
 import { PostCommentComponent } from './components/post/comments/post-comment/post-comment.component';
 import { PostCommentsComponent } from './components/post/comments/post-comments/post-comments.component';
+import { InputSendComponent } from './components/shared/input-send/input-send.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { PostCommentsComponent } from './components/post/comments/post-comments/
     PostPageComponent,
     FileUploadComponent,
     PostCommentComponent,
-    PostCommentsComponent
+    PostCommentsComponent,
+    InputSendComponent
   ],
   imports: [BrowserModule, AppRoutingModule, SharedModule, MaterialModule, MatButtonModule, MatCardModule],
   providers: [],
