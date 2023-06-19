6143338384bc2b71d3ae1101941d5283733a9ace
radubuzas <radu-gabriel.buzas@s.unibuc.ro>
2023-06-19 15:52:18 +0300

    Routing...

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { UserService } from 'app/core/services/user.service';
 import { Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
+import {Router, Routes} from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { SessionUser } from 'app/models/session-user.model';
 import { handleError } from 'app/shared/utils/error';
+import {ApiTestsComponent} from "../api-tests/api-tests.component";
+import {LoginFormComponent} from "../login/login-form.component";
+import {RegisterFormComponent} from "../register/register-form.component";
+import {CreatePostComponent} from "../post/create-post/create-post.component";
+import {IsLoggedInGuard} from "../../shared/guards/is-logged-in.guard";
+import {PostPageComponent} from "../post-page/post-page.component";
+import {CreateProfileComponent} from "../create-profile/create-profile.component";
+import {EditProfileComponent} from "../edit-profile/edit-profile.component";
+import {FeedComponent} from "../feed/feed.component";
+import {ShowProfileComponent} from "../show-profile/show-profile.component";
 
 @Component({
   selector: 'mds-navbar',
@@ -18,6 +28,20 @@ export class NavbarComponent {
   private readonly router: Router = new Router();
   constructor(private readonly userService: UserService) {}
 
+  private routs: Routes = [
+    { path: 'api-tests', component: ApiTestsComponent },
+    { path: 'login', component: LoginFormComponent },
+    { path: 'register', component: RegisterFormComponent },
+    { path: 'posts/create', component: CreatePostComponent, canActivate: [IsLoggedInGuard] },
+    { path: 'posts/:id', component: PostPageComponent },
+    { path: 'create-profile', component: CreateProfileComponent, canActivate: [IsLoggedInGuard] },
+    { path: 'edit-profile', component: EditProfileComponent, canActivate: [IsLoggedInGuard] },
+    { path: 'feed', component: FeedComponent, canActivate: [IsLoggedInGuard] },
+    { path: 'posts/:id', component: PostPageComponent },
+    { path: 'profile/:userId', component: ShowProfileComponent },
+    { path: '', redirectTo: '/feed', pathMatch: 'full' },
+  ];
+
   public logout() {
     this.userService.logout().subscribe();
 
@@ -26,4 +50,5 @@ export class NavbarComponent {
       .pipe(catchError(handleError()))
       .subscribe(() => this.router.navigate(['/']));
   }
+
 }
