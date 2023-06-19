e11f847024f2bc8bb006815584d2ede99365774e
radubuzas <radu-gabriel.buzas@s.unibuc.ro>
2023-06-19 13:11:21 +0300

    Routing when logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { UserService } from 'app/core/services/user.service';
-import { Observable } from 'rxjs';
+import {Observable, throwError} from 'rxjs';
+import {reportUnhandledError} from "rxjs/internal/util/reportUnhandledError";
+import {Router} from "@angular/router";
+import {catchError} from "rxjs/operators";
 
 @Component({
   selector: 'mds-navbar',
@@ -10,12 +13,23 @@ import { Observable } from 'rxjs';
 export class NavbarComponent {
   public readonly isLoggedIn$: Observable<boolean> = this.userService.isLoggedIn$;
 
-
+  private readonly router: Router = new Router();
   constructor(private readonly userService: UserService) {}
 
   public logout() {
     this.userService.logout().subscribe();
-    //redirect to login page
 
+    function handleError(error: any) {
+      // Handle the error here
+      console.error('An error occurred:', error);
+      // Optionally, rethrow the error to propagate it further
+      return throwError(error);
+    }
+
+    this.userService.logout().
+      pipe(
+        catchError(handleError)
+      )
+      .subscribe(() => this.router.navigate(['/login']));
   }
 }
